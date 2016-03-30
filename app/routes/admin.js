0284113c8f5caba65ecfadf42ea7fc162273275f
Isabelle George and Neil Larion <neil.larion@gmail.com>
2016-03-30 10:05:02 -0700

    add ability to delete entries

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,12 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+  model() {
+    return this.store.findAll('post');
+  },
+
   actions: {
     submitEntry(blogParams){
       var newEntry = this.store.createRecord('post', blogParams);
       newEntry.save();
       this.transitionTo('admin');
+    },
+    deleteEntry(post){
+      if(confirm("Delete entry?")){
+        post.destroyRecord();
+        this.transitionTo('admin');
+      }
     }
   }
 });
